020d57991c7eca4486f4ad40e8afba98a91cb0a6
mubarakbdlrf <mubarakbdlrf@gmail.com>
2024-08-17 09:57:53 +0000

    added Bitzsales

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,13 +3,14 @@ import "./portfolio.css";
 import AOS from "aos";
 import Github from "../../assets/GithubSearch.JPG";
 import Img1 from "../../assets/MinuteApp.PNG";
-import Img2 from "../../assets/Home.JPG";
+import socialUi from "../../assets/socialUi.png";
 import Img3 from "../../assets/wages.PNG";
 import Img6 from "../../assets/IgClone.JPG";
 import React from "react";
 import blog from "../../assets/blog.JPG";
 import wordle from "../../assets/wordle.JPG";
 import ecommerce from "../../assets/ecommerce.png"
+import bitzsales from "../../assets/bitzsales.png"
 
 // ..
 AOS.init({
@@ -23,40 +24,48 @@ const data = [
     title: "Ecommerce Website",
     github: "https://github.com/BINARY-TECH001/E_commerceClient",
     desc: "I built a React.js eCommerce platform with a responsive UI, real-time updates, and smooth checkout. Features include a customizable catalog, secure payments, and an intuitive admin dashboard, ensuring a seamless user experience.",
-    demo: "#"
+    demo: "https://ecommbybinary.vercel.app/"
   },
   {
-    id: 10,
-    img: Img1,
-    title: "Meeting Minutes Management System",
-    github: "https://github.com/MinuteApp",
-    desc: "A web application for taking meetingss minutes by recording the meeting audio and video and transcribing meeting audio text to generate meeting minutes. It also helps in keeping, retrieving and managing meeting minutes.",
-    demo: "#"
+    id: 'Ecommerce Admin',
+    img: bitzsales,
+    title: "Ecommerce Admin Management System",
+    github: "https://github.com/BINARY-TECH001/Bitzsales",
+    desc: "I Developed an eCommerce Admin Management System with Next.js, Tailwind CSS, and MongoDB. It features a sleek UI, efficient inventory management, real-time updates, and secure data handling for optimal administrative control.",
+    demo: "https://bitzsales.vercel.app/"
   },
   {
     id: 2,
-    img: Img2,
+    img: socialUi,
     title: "Binary Social Media Application UI",
     github: "https://github.com/BINARY-TECH001/SocialMedia-UI",
     desc: "A Web Social media User Interface designed using React.Js. It contains the authentication pages (Login & Register), Story/Home page, comment, like, post and profile page.",
     demo: "https://socialuibybinary.vercel.app/"
   },
-  {
-    id: 3,
-    img: Img3,
-    title: "Employees wages Calculator & Report",
-    github: "https://github.com//BINARY-TECH001/",
-    desc: "A web based application designed using PHP that helps manage employee wages and calculations.",
-    demo: "#"
-  },
-  {
-    id: 6,
-    img: Img6,
-    title: "Instagram Page Clone Using React & Dexie",
-    github: "https://github.com/BINARY-TECH001/ig-clone/",
-    desc: "Cloned the Instagram profile page using React.Js and the Dexie for storage.",
-    demo: "https://binarytech-IgClone.netlify.app"
-  },
+  // {
+  //   id: 10,
+  //   img: Img1,
+  //   title: "Meeting Minutes Management System",
+  //   github: "https://github.com/MinuteApp",
+  //   desc: "A web application for taking meetingss minutes by recording the meeting audio and video and transcribing meeting audio text to generate meeting minutes. It also helps in keeping, retrieving and managing meeting minutes.",
+  //   demo: "#"
+  // },
+  // {
+  //   id: 3,
+  //   img: Img3,
+  //   title: "Employees wages Calculator & Report",
+  //   github: "https://github.com//BINARY-TECH001/",
+  //   desc: "A web based application designed using PHP that helps manage employee wages and calculations.",
+  //   demo: "#"
+  // },
+  // {
+  //   id: 6,
+  //   img: Img6,
+  //   title: "Instagram Page Clone Using React & Dexie",
+  //   github: "https://github.com/BINARY-TECH001/ig-clone/",
+  //   desc: "Cloned the Instagram profile page using React.Js and the Dexie for storage.",
+  //   demo: "https://binarytech-IgClone.netlify.app"
+  // },
   {
     id: 7,
     img: blog,
